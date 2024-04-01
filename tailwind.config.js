7ffddd100b34305e333f8badb7b66d20129fea36
syaifullohismail <syaifullohismail123@gmail.com>
2024-04-02 00:01:10 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "abu-abu": "#9EA0A5",
+        "dongker-muda": "#46505C",
+        "kuning-muda": "#FBB0175E",
+        'ungu-muda' : '#5E50A1'
+      },
+    },
+  },
+  plugins: [],
+};
